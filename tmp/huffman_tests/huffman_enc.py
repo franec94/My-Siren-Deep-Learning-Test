6f71e0c8d1913c20da96a50a039ad141e7335f9f
franec94 <franec94@gmail.com>
2020-10-01 20:58:41 +0200

    Refactoring code for huffman coding technique tests

--- tmp/huffman_tests/huffman_enc.py
+++ tmp/huffman_tests/huffman_enc.py
@@ -23,13 +23,9 @@ import time
 # import visdom
 import warnings
 
-
-parser = argparse.ArgumentParser(description='Custom Huffman Encoding Script')
-parser.add_argument('--input-file', type=str, dest="input_file",
-                    help='Location of input resource to be compressed, from local file system.')
-parser.add_argument('--output-dest', type=str, dest="output_file",
-                    help='Location of output compressed result, into local file system.')
-
+# --------------------------------------------- #
+# Utils
+# --------------------------------------------- #
 
 def get_stats_words_sorted(lines):
     words = dict()
@@ -43,6 +39,9 @@ def get_stats_words_sorted(lines):
     sorted_words = dict(sorted(words.items(), key = lambda x: x[1], reverse = False))
     return sorted_words
 
+# --------------------------------------------- #
+# Tree Section
+# --------------------------------------------- #
 
 def create_node(item):
     return {
@@ -56,9 +55,9 @@ def create_node(item):
 
 def create_pqueue(a_dict):
     p_queue = list(map(create_node, a_dict.items()))
-
-    pprint(p_queue)
+    # pprint(p_queue)
     return p_queue
+
 
 def build_tree(p_queue):
     while len(p_queue) != 1:
@@ -94,6 +93,43 @@ def build_tree(p_queue):
     return p_queue[0]
 
 
+def crea_huffman_tree_from_file(input_filename):
+    lines = None
+    with open(input_filename, "r") as f:
+        lines = f.read().split("\n")
+
+    sorted_words = get_stats_words_sorted(lines)
+    
+    p_queue = create_pqueue(sorted_words)
+    tree = build_tree(p_queue)
+    return tree
+
+
+def pre_order_tree_traversal(tree, a_code):
+    if tree['l'] == None and tree['r'] == None:
+        print(tree['item'], "--->", a_code + tree['code'])
+
+    if tree['code'] != None:
+        tmp_code = a_code + tree["code"]
+    else:
+        tmp_code = a_code
+    if tree['l'] != None:
+        pre_order_tree_traversal(tree['l'], tmp_code)
+    if tree['r'] != None:
+        pre_order_tree_traversal(tree['r'], tmp_code)
+    pass
+    
+    pass
+
+
+def show_encoding(tree):
+    pre_order_tree_traversal(tree, "")
+    pass
+
+# --------------------------------------------- #
+# Encoding & Decoding Sections
+# --------------------------------------------- #
+
 def get_code(tree, item):
     if tree['l'] == None and tree['r'] == None:
         if tree['item'] == item: return tree['code']
@@ -115,23 +151,8 @@ def get_code(tree, item):
                 return code
     return None
 
-def encode_file(input_filename, output_filename):
-    lines = None
-    with open(input_filename, "r") as f:
-        lines = f.read().split("\n")
 
-    sorted_words = get_stats_words_sorted(lines)
-    
-    p_queue = create_pqueue(sorted_words)
-    tree = build_tree(p_queue)
-
-    pprint(tree)
-
-    all_words = list(sorted_words.keys())
-        
-    for a_word in all_words:
-        a_code = get_code(tree, a_word)
-        print(a_word, "--->", a_code)
+def encode_file(tree, input_filename, output_filename):
     with open(input_filename, "r") as fin:
         with open(output_filename, "w") as fout:
             for a_line in fin.read().split("\n"):
@@ -141,7 +162,7 @@ def encode_file(input_filename, output_filename):
                     pass
                 pass
             print("", file=fout)
-    return tree
+    pass
 
 
 def decode_stream(a_stream, tree, pos):
@@ -150,6 +171,7 @@ def decode_stream(a_stream, tree, pos):
     if a_stream[pos] == "0":
         return decode_stream(a_stream, tree['l'], pos + 1)
     return decode_stream(a_stream, tree['r'], pos + 1)
+
 
 def decode_file(tree, input_filename):
     with open(input_filename, "r") as fin:
@@ -161,32 +183,4 @@ def decode_file(tree, input_filename):
                 print(symbol, sep="", end="")
                 if pos > len(a_line): break
             print("")
-    pass
-
-def main(args):
-
-    input_filename = args.input_file
-    output_filename = args.output_file
-
-    if os.path.exists(input_filename) is False:
-        print(f"Error: {input_filename} does not exists!", file=sys.stderr)
-        return -1
-    if os.path.isfile(input_filename) is False:
-        print(f"Error: {input_filename} is not a file!", file=sys.stderr)
-        return -1
-    
-    tree = encode_file(input_filename, output_filename)
-    # pprint(tree)
-
-    decode_file(tree, output_filename)
-    
-    return 0
-
-if __name__ == "__main__":
-
-    # Parse input arguments
-    args, unknown = parser.parse_known_args()
-
-    exit_code = main(args)
-    sys.exit(exit_code)
     pass
---
+++ tmp/huffman_tests/main_test_txt2enc.py
@@ -0,0 +1,73 @@
+# ============================================= #
+# Standard Imports
+# ============================================= #
+from __future__ import print_function
+from __future__ import division
+
+from collections import  Counter
+from pprint import pprint
+from PIL import Image
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import argparse
+import copy
+import datetime
+import os
+import random
+import skimage
+import shutil
+import sys
+import time
+# import visdom
+import warnings
+
+from huffman_enc import encode_file, decode_file, crea_huffman_tree_from_file, show_encoding
+
+parser = argparse.ArgumentParser(description='Custom Huffman Encoding Script')
+parser.add_argument('--input-file', type=str, dest="input_file",
+                    help='Location of input resource to be compressed, from local file system.')
+parser.add_argument('--output-dest', type=str, dest="output_file",
+                    help='Location of output compressed result, into local file system.')
+
+
+def main(args):
+
+    input_filename = args.input_file
+    output_filename = args.output_file
+
+    if os.path.exists(input_filename) is False:
+        print(f"Error: {input_filename} does not exists!", file=sys.stderr)
+        return -1
+    if os.path.isfile(input_filename) is False:
+        print(f"Error: {input_filename} is not a file!", file=sys.stderr)
+        return -1
+    
+    tree = crea_huffman_tree_from_file(input_filename)
+
+    # pprint(tree)
+    print('Symbol --> Code')
+    print('-' * 40)
+    show_encoding(tree)
+    print()
+
+    print('Encoded File')
+    print('-' * 40)
+    encode_file(tree, input_filename, output_filename)
+    print()
+    
+    print('Dencoded File')
+    print('-' * 40)
+    decode_file(tree, output_filename)
+    
+    return 0
+
+if __name__ == "__main__":
+
+    # Parse input arguments
+    args, unknown = parser.parse_known_args()
+
+    exit_code = main(args)
+    sys.exit(exit_code)
+    pass
