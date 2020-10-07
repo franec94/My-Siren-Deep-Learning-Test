69281483b41efb3c9d9c4b3f0391c428b97716c2
franec94 <franec94@gmail.com>
2020-10-07 12:29:38 +0200

    Fixed minor bugs

--- dev-cmd-line-tools/compress-image-siren/src/utils/torchmeta/utils/__init__.py
+++ dev-cmd-line-tools/compress-image-siren/src/utils/torchmeta/utils/__init__.py
@@ -1,3 +1,3 @@
-from torchmeta.utils import data
-from torchmeta.utils.metrics import hardness_metric
-from torchmeta.utils.prototype import get_num_samples, get_prototypes, prototypical_loss
+from src.utils.torchmeta.utils import data
+from src.utils.torchmeta.utils.metrics import hardness_metric
+from src.utils.torchmeta.utils.prototype import get_num_samples, get_prototypes, prototypical_loss
