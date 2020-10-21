4875b930bd5ee39b45218b18ab1100a89a9e26c5
franec94 <franec94@gmail.com>
2020-10-22 01:53:38 +0200

    Updates.

--- dev-cmd-line-tools/compute-analysis-jpeg-compression/run_examples.py
+++ dev-cmd-line-tools/compute-analysis-jpeg-compression/run_examples.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+clear
+echo "Compute analysis for Camera (default) image:"
+python3 \
+  compute_analysis_jpeg_compression.py \
+  --output_path camera/output \
+  --logging_path camera/log
+
+echo "Compute analysis for test068.png image:"
 python3 \
   compute_analysis_jpeg_compression.py \
   --input_image ../../testsets/BSD68/test068.png \
