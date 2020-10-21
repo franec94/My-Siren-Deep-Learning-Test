6f09c791cf0f7def6f9305fac0d43f6ca7f467d2
franec94 <franec94@gmail.com>
2020-10-21 22:49:14 +0200

    Updates.

--- dev-cmd-line-tools/compare-extended-compression-choices/src/utils/train_extended_compare.py
+++ dev-cmd-line-tools/compare-extended-compression-choices/src/utils/train_extended_compare.py
@@ -388,11 +388,11 @@ def train_extended_protocol_compare_archs(grid_arch_hyperparams, img_dataset, op
                     tqdm.write(
                         "Arch no.=%d, Trial no.=%d, loss=%0.6f, PSNR=%0.6f, SSIM=%0.6f, eta=%0.6f"
                         % (arch_no, trial_no, train_losses[0], train_losses[1], train_losses[2], stop_time))
-                    logging.info(
+                    pass
+                logging.info(
                         "Arch no.=%d, Trial no.=%d, loss=%0.6f, PSNR=%0.6f, SSIM=%0.6f, eta=%0.6f"
                         % (arch_no, trial_no, train_losses[0], train_losses[1], train_losses[2], stop_time)
                     )
-                    pass
                 
 
                 # --- Record performance metrices for later investigations.
