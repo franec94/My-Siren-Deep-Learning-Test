19e63d128f5558034760db6461d9efa40bcb0233
franec94 <franec94@gmail.com>
2020-10-23 04:52:51 +0200

    Updated run_example.py src file

--- dev-cmd-line-tools/compare-extended-compression-choices/run_example.sh
+++ dev-cmd-line-tools/compare-extended-compression-choices/run_example.sh
@@ -3,11 +3,11 @@
 CUDA_VISIBLE_DEVICES=0 python main_extended_compare.py \
   --logging_root '../../../../results' \
   --experiment_name 'train' \
-  --num_epochs 500 \
+  --num_epochs 100000 \
   --hidden_features 64 \
   --num_attempts 1 \
-  --seeds 0 42 123 1234\
-  --hidden_layers 5 7 8 9 10 11 12 13 \
+  --seeds 0 42 123 \
+  --hidden_layers 10 11 12 13 \
   --resume_from 0 \
   --end_to  1 \
   --verbose 1 \
