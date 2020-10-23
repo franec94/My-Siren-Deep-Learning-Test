48554d3bd365624c00725b00b47c985a474698cf
franec94 <franec94@gmail.com>
2020-10-23 20:30:05 +0200

    Added Makefile

--- dev-cmd-line-tools/compare-extended-compression-choices/run_program.sh
+++ dev-cmd-line-tools/compare-extended-compression-choices/run_program.sh
@@ -4,12 +4,12 @@ CUDA_VISIBLE_DEVICES=0 python main_extended_compare.py \
   --logging_root '../../../../results' \
   --experiment_name 'train' \
   --num_epochs 100000 \
-  --hidden_features 64 \
+  --hidden_features 75 \
   --num_attempts 1 \
   --seeds 0 42 123 \
   --hidden_layers 10 11 12 13 \
   --resume_from 0 \
-  --end_to  1 \
+  --end_to  5 \
   --enable_tensorboard_logging \
   --verbose 1 \
   --show_number_of_trials \
