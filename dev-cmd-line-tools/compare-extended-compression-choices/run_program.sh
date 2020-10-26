17bb5e587f6b475ad7702a49fc579cec4c36d1a4
franec94 <franec94@gmail.com>
2020-10-26 18:00:27 +0100

    Starting a new run with hf 85 and sidelength 256.

--- dev-cmd-line-tools/compare-extended-compression-choices/run_program.sh
+++ dev-cmd-line-tools/compare-extended-compression-choices/run_program.sh
@@ -17,7 +17,7 @@ CUDA_VISIBLE_DEVICES=0 python main_extended_compare.py \
   --hidden_features 85 \
   --num_attempts 1 \
   --seeds 0 42 123 \
-  --hidden_layers 3 4 5 6 7 8 9 \
+  --hidden_layers 10 11 12 13 \
   --resume_from 0 \
   --end_to  5 \
   --enable_tensorboard_logging \
