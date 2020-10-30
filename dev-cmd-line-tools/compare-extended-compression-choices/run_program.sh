1b2a3a30acc1520693b446a76597f082701f5a91
franec94 <franec94@gmail.com>
2020-10-30 23:35:39 +0100

    new run for hf=35 sd=256

--- dev-cmd-line-tools/compare-extended-compression-choices/run_program.sh
+++ dev-cmd-line-tools/compare-extended-compression-choices/run_program.sh
@@ -14,10 +14,10 @@ CUDA_VISIBLE_DEVICES=0 python main_extended_compare.py \
   --experiment_name 'train' \
   --sidelength 256 \
   --num_epochs 100000 \
-  --hidden_features 90 \
+  --hidden_features 35 \
   --num_attempts 1 \
-  --seeds 0 42 123 \
-  --hidden_layers 3 4 5 6 7 8 9 \
+  --seeds 0 42 123 1234 101 \
+  --hidden_layers 2 3 4 5 6 7 8 9 \
   --resume_from 0 \
   --end_to  1 \
   --enable_tensorboard_logging \
