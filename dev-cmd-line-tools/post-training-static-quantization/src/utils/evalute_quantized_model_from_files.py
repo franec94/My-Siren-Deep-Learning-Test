b7f500d958810e56c3baaa13673e336608c9bc4c
franec94 <franec94@gmail.com>
2020-11-08 15:33:18 +0100

    Updates while workign on post train quant

--- dev-cmd-line-tools/post-training-static-quantization/src/utils/evalute_quantized_model_from_files.py
+++ dev-cmd-line-tools/post-training-static-quantization/src/utils/evalute_quantized_model_from_files.py
@@ -98,10 +98,13 @@ def _evaluate_model_local(image_dataset, model_conf, quant_tech = None, device =
     eval_scores = []
     if quant_tech == None:
         try:
+            print("Try eval plain model on cuda device...")
             eval_dataloader = _prepare_data_loaders(image_dataset, model_conf)
             model = prepare_model(opt = model_conf, arch_hyperparams=model_conf._asdict(), device='cuda')
             eval_scores = _evaluate_model(model, evaluate_dataloader=eval_dataloader, device='cuda')
         except:
+            print("No cuda device available, switching to cpu.")
+            print("Try eval plain model on cpu device...")
             eval_dataloader = _prepare_data_loaders(image_dataset, model_conf)
             model = prepare_model(opt = model_conf, arch_hyperparams=model_conf._asdict(), device='cpu')
             eval_scores = _evaluate_model(model, evaluate_dataloader=eval_dataloader, device='cpu')
