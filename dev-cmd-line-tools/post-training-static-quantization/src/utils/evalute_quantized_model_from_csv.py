2f0eca8714d6220359e73fbe20fba18b10e6fb85
franec94 <franec94@gmail.com>
2020-11-08 17:53:18 +0100

    Updates while workign on post train quant

--- dev-cmd-line-tools/post-training-static-quantization/src/utils/evalute_quantized_model_from_csv.py
+++ dev-cmd-line-tools/post-training-static-quantization/src/utils/evalute_quantized_model_from_csv.py
@@ -338,7 +338,7 @@ def evaluate_post_train_quantized_models_by_csv_2(a_file_csv, args, device = 'cp
         
         eval_scores = _evaluate_model_local(image_dataset = img_dataset, model_conf = model_conf, quant_tech = None, device = 'cuda')
 
-        vals_r = [vals.path, int(vals.hl), int(vals.hf), opt.sidelength, None] + list(eval_scores)
+        vals_r = [vals.path, int(vals.hl), int(vals.hf), opt.sidelength, 'None'] + list(eval_scores)
         a_record = InfoResults._make(vals_r)
         records_list.append(a_record)
 
