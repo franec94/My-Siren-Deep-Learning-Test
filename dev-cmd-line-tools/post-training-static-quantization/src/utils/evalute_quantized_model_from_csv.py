990bc4f512077d1be693285c676ad390b5237dc1
franec94 <franec94@gmail.com>
2020-11-09 15:34:17 +0100

    Minor changes.

--- dev-cmd-line-tools/post-training-static-quantization/src/utils/evalute_quantized_model_from_csv.py
+++ dev-cmd-line-tools/post-training-static-quantization/src/utils/evalute_quantized_model_from_csv.py
@@ -88,20 +88,22 @@ def _evaluate_model_local(image_dataset, model_conf, quant_tech = None, device =
             print("Try eval plain model on cuda device...")
             eval_dataloader = _prepare_data_loaders(image_dataset, model_conf)
             model = prepare_model(opt = model_conf, arch_hyperparams=model_conf._asdict(), device='cuda', model_weights_file = model_conf.model_filename)
-            eval_scores = _evaluate_model_2(model, evaluate_dataloader=eval_dataloader, device='cuda')
+            size_model = get_size_of_model(model)
+            eval_scores, eval_time = _evaluate_model_2(model, evaluate_dataloader=eval_dataloader, device='cuda')
         else:
             print("No cuda device available, switching to cpu.")
             print("Try eval plain model on cpu device...")
             eval_dataloader = _prepare_data_loaders(image_dataset, model_conf)
             model = prepare_model(opt = model_conf, arch_hyperparams=model_conf._asdict(), device='cpu',  model_weights_file = model_conf.model_filename)
-            eval_scores = _evaluate_model_2(model, evaluate_dataloader=eval_dataloader, device='cpu')
+            size_model = get_size_of_model(model)
+            eval_scores, eval_time = _evaluate_model_2(model, evaluate_dataloader=eval_dataloader, device='cpu')
         pass
     else:
         print('Eval:', quant_tech.upper())
-        eval_scores = compute_quantization(img_dataset = image_dataset, opt = model_conf, model_path = model_conf.model_filename, arch_hyperparams = model_conf._asdict(), fuse_modules = None, device = 'cpu', qconfig = 'fbgemm')
+        eval_scores, eta_eval, size_model = compute_quantization(img_dataset = image_dataset, opt = model_conf, model_path = model_conf.model_filename, arch_hyperparams = model_conf._asdict(), fuse_modules = None, device = 'cpu', qconfig = 'fbgemm')
         pass
 
-    return eval_scores
+    return eval_scores, eta_eval, size_model
 
 
 def _print_size_of_model(model):
