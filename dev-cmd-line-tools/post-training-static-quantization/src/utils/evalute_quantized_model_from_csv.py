4971aa01bcb6ada728b036759620146bfaedbf43
franec94 <franec94@gmail.com>
2020-11-09 15:32:31 +0100

    Minor changes.

--- dev-cmd-line-tools/post-training-static-quantization/src/utils/evalute_quantized_model_from_csv.py
+++ dev-cmd-line-tools/post-training-static-quantization/src/utils/evalute_quantized_model_from_csv.py
@@ -93,7 +93,7 @@ def _evaluate_model_local(image_dataset, model_conf, quant_tech = None, device =
             print("No cuda device available, switching to cpu.")
             print("Try eval plain model on cpu device...")
             eval_dataloader = _prepare_data_loaders(image_dataset, model_conf)
-            model = prepare_model(opt = model_conf, arch_hyperparams=model_conf._asdict(), device='cpu')
+            model = prepare_model(opt = model_conf, arch_hyperparams=model_conf._asdict(), device='cpu',  model_weights_file = model_conf.model_filename)
             eval_scores = _evaluate_model_2(model, evaluate_dataloader=eval_dataloader, device='cpu')
         pass
     else:
