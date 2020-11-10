bf7916473c098b14b51bc18c221fae1bea270e56
franec94 <franec94@gmail.com>
2020-11-10 23:12:18 +0100

    Minor changes.

--- dev-cmd-line-tools/post-training-static-quantization/src/utils/evalute_quantized_model_from_csv.py
+++ dev-cmd-line-tools/post-training-static-quantization/src/utils/evalute_quantized_model_from_csv.py
@@ -328,7 +328,7 @@ def evaluate_post_train_quantized_models_by_csv_2(a_file_csv, args, device = 'cp
         quant_tech_list = []
 
     records_list = []
-    for row in cropped_images_df[-2:-1:-1].values:
+    for row in cropped_images_df[-2:-1:].values:
         vals = Columns._make(row)
 
         if os.path.exists(vals.path) is False or os.path.isfile(vals.path) is False:
