4bab2303ee4838ce745bfe21d3a0e5c9d42b6572
franec94 <franec94@gmail.com>
2020-11-10 16:12:07 +0100

    Minor changes.

---
+++ dev-cmd-line-tools/post-training-static-quantization/src/utils/archs/siren_custom_quant.py
@@ -0,0 +1,202 @@
+from __future__ import print_function
+from __future__ import division
+
+# --------------------------------------------- #
+# Standard Library, plus some Third Party Libraries
+# --------------------------------------------- #
+
+import logging
+from pprint import pprint
+from PIL import Image
+from tqdm import tqdm
+from typing import Union, Tuple
+
+import configargparse
+from functools import partial
+
+import copy
+import collections
+import datetime
+import functools
+import h5py
+import math
+import os
+import operator
+import pickle
+import random
+import shutil
+import sys
+import re
+import time
+import tabulate
+
+import torch
+import torchvision
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+
+from torchvision import datasets
+from torchvision import transforms
+from torchvision.transforms import Resize, Compose, ToTensor, Normalize
+
+
+from collections import OrderedDict
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+from src.utils.quant_utils.quant_utils_functions_2 import gather_stats as gather_stats_inner
+from src.utils.quant_utils.quant_utils_functions_2 import quantize_tensor, quantize_layer, QTensor, dequantize_tensor
+
+class SineLayerCQ(nn.Module):
+    # See paper sec. 3.2, final paragraph, and supplement Sec. 1.5 for discussion of omega_0.
+    
+    # If is_first=True, omega_0 is a frequency factor which simply multiplies the activations before the 
+    # nonlinearity. Different signals may require different omega_0 in the first layer - this is a 
+    # hyperparameter.
+    
+    # If is_first=False, then the weights will be divided by omega_0 so as to keep the magnitude of 
+    # activations constant, but boost gradients to the weight matrix (see supplement Sec. 1.5)
+    
+    def __init__(self, in_features, out_features, bias=True,
+                 is_first=False, omega_0=30, prev_layer_name = None, succ_layer_name = None, quant = False):
+        super().__init__()
+        self.omega_0 = omega_0
+        self.quant = quant
+        self.prev_layer_name = prev_layer_name
+        self.succ_layer_name = None
+        self.is_first = is_first
+        
+        self.in_features = in_features
+        self.linear = nn.Linear(in_features, out_features, bias=bias)
+        
+        self.init_weights()
+        pass
+    
+    def init_weights(self):
+        with torch.no_grad():
+            if self.is_first:
+                self.linear.weight.uniform_(-1 / self.in_features, 
+                                             1 / self.in_features)      
+            else:
+                self.linear.weight.uniform_(-np.sqrt(6 / self.in_features) / self.omega_0, 
+                                             np.sqrt(6 / self.in_features) / self.omega_0)
+        pass
+        
+    def forward(self, input):
+        return torch.sin(self.omega_0 * self.linear(input))
+    
+    def forward_with_intermediate(self, input): 
+        # For visualization of activation distributions
+        intermediate = self.omega_0 * self.linear(input)
+        return torch.sin(intermediate), intermediate
+    pass
+    
+    
+class SirenCQ(nn.Module):
+    def __init__(self, in_features, hidden_features, hidden_layers, out_features, outermost_linear=False, 
+                 first_omega_0=30, hidden_omega_0=30., quant = False):
+        super().__init__()
+        
+        self.net = []
+        self.quant = quant
+        self.stats = {}
+        a_layer = SineLayerCQ(in_features, hidden_features, \
+                                  quant=quant, \
+                                  is_first=True, omega_0=first_omega_0)
+        self.net.append(a_layer)
+
+        for i in range(hidden_layers):
+            prev_layer_name = a_layer._get_name()
+            curr_layer = SineLayerCQ(hidden_features, hidden_features, \
+                                      quant=quant, \
+                                      is_first=False, omega_0=hidden_omega_0, prev_layer_name=prev_layer_name)
+            a_layer.succ_layer_name = curr_layer._get_name()
+            self.net.append(curr_layer)
+
+        if outermost_linear:
+            final_linear = nn.Linear(hidden_features, out_features)
+            
+            with torch.no_grad():
+                final_linear.weight.uniform_(-np.sqrt(6 / hidden_features) / hidden_omega_0, 
+                                              np.sqrt(6 / hidden_features) / hidden_omega_0)
+                
+            self.net.append(final_linear)
+        else:
+            self.net.append(SineLayerCQ(hidden_features, out_features, 
+                                      is_first=False, omega_0=hidden_omega_0))
+        
+        self.net = nn.Sequential(*self.net)
+        pass
+    
+
+    def forward(self, coords):
+        coords = coords.clone().detach().requires_grad_(True) # allows to take derivative w.r.t. input
+        if self.quant == False:
+            output = self.net(coords)
+        else:
+            output = self._forward_quantized(coords)
+            pass
+        return output, coords        
+    
+
+    def _forward_quantized(self, x):
+        for a_module in self.net:
+            if type(a_module) == SineLayerCQ:
+                if a_module.is_first:
+                    x = quantize_tensor(x, min_val=self.stats[f'{a_module._get_name()}']['min'], max_val=self.stats[f'{a_module._get_name()}']['max'])
+                    pass
+                x, scale_next, zero_point_next = quantize_layer(x.tensor, a_module, self.stats[f'{a_module.succ_layer_name}'], x.scale, x.zero_point)
+            if type(a_module) == nn.Linear:
+                x = dequantize_tensor(QTensor(tensor=x, scale=scale_next, zero_point=zero_point_next))
+                x = a_module(x)
+            pass
+        return x
+
+
+    def forward_with_activations(self, coords, retain_grad=False):
+        '''Returns not only model output, but also intermediate activations.
+        Only used for visualizing activations later!'''
+        activations = OrderedDict()
+
+        activation_count = 0
+        x = coords.clone().detach().requires_grad_(True)
+        activations['input'] = x
+        for i, layer in enumerate(self.net):
+            if isinstance(layer, SirenCQ):
+                x, intermed = layer.forward_with_intermediate(x)
+                
+                if retain_grad:
+                    x.retain_grad()
+                    intermed.retain_grad()
+                    
+                activations['_'.join((str(layer.__class__), "%d" % activation_count))] = intermed
+                activation_count += 1
+            else: 
+                x = layer(x)
+                
+                if retain_grad:
+                    x.retain_grad()
+                    
+            activations['_'.join((str(layer.__class__), "%d" % activation_count))] = x
+            activation_count += 1
+
+        return activations
+
+
+    def gather_stats(self, test_loader):
+        """Gather stats for quantizing computations, inferences.
+        Params:
+        -------
+        :test_loader: DatasetLoader from Pytorch framework.\n
+        Return:
+        -------
+        :stats_collected: python dictionary object,copied and retrieved with statistics collected.\n
+        """
+        stats_collected = gather_stats_inner(self, test_loader, self.device().type)
+        self.stats = stats_collected
+        return copy.deepcopy(stats_collected)
+    pass
---
+++ dev-cmd-line-tools/post-training-static-quantization/src/utils/quant_utils/quant_utils_functions_2.py
@@ -0,0 +1,230 @@
+from __future__ import print_function
+from __future__ import division
+
+# --------------------------------------------- #
+# Standard Library, plus some Third Party Libraries
+# --------------------------------------------- #
+
+import logging
+from pprint import pprint
+from PIL import Image
+from tqdm import tqdm
+from typing import Union, Tuple
+
+import configargparse
+from functools import partial
+
+import copy
+import collections
+import datetime
+import functools
+import h5py
+import math
+import os
+import operator
+import pickle
+import random
+import shutil
+import sys
+import re
+import time
+import tabulate
+# import visdom
+
+# --------------------------------------------- #
+# Data Science and Machine Learning Libraries
+# --------------------------------------------- #
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.style.use('ggplot')
+
+import numpy as np
+import pandas as pd
+import sklearn
+# ----------------------------------------------------------------------------------------------- #
+# Imports
+# ----------------------------------------------------------------------------------------------- #
+from collections import namedtuple
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from src.utils.archs.siren_custom_quant import SineLayerCQ, SirenCQ
+
+# ----------------------------------------------------------------------------------------------- #
+# Objects
+# ----------------------------------------------------------------------------------------------- #
+QTensor = namedtuple('QTensor', ['tensor', 'scale', 'zero_point'])
+
+
+# ----------------------------------------------------------------------------------------------- #
+# Functions
+# ----------------------------------------------------------------------------------------------- #
+def calc_scale_zero_point(min_val, max_val,num_bits=8):
+  # Calc Scale and zero point of next 
+  qmin = 0.
+  qmax = 2.**num_bits - 1.
+
+  scale = (max_val - min_val) / (qmax - qmin)
+
+  initial_zero_point = qmin - min_val / scale
+  
+  zero_point = 0
+  if initial_zero_point < qmin:
+      zero_point = qmin
+  elif initial_zero_point > qmax:
+      zero_point = qmax
+  else:
+      zero_point = initial_zero_point
+
+  zero_point = int(zero_point)
+
+  return scale, zero_point
+
+
+def quantize_tensor(x, num_bits=8, min_val=None, max_val=None):
+    
+    if not min_val and not max_val: 
+      min_val, max_val = x.min(), x.max()
+
+    qmin = 0.
+    qmax = 2.**num_bits - 1.
+
+    scale, zero_point = calc_scale_zero_point(min_val, max_val, num_bits)
+    q_x = zero_point + x / scale
+    q_x.clamp_(qmin, qmax).round_()
+    q_x = q_x.round().byte()
+    
+    return QTensor(tensor=q_x, scale=scale, zero_point=zero_point)
+
+
+def dequantize_tensor(q_x):
+    return q_x.scale * (q_x.tensor.float() - q_x.zero_point)
+
+# --------------------------------------------- #
+# Functions
+# --------------------------------------------- #
+
+def quantize_layer(x, layer, stat, scale_x, zp_x):
+  # for both conv and linear layers
+  W = layer.weight.data
+  B = layer.bias.data
+
+  # scale_x = x.scale
+  # zp_x = x.zero_point
+  w = quantize_tensor(layer.weight.data) 
+  b = quantize_tensor(layer.bias.data)
+
+  layer.weight.data = w.tensor.float()
+  layer.bias.data = b.tensor.float()
+
+  scale_w = w.scale
+  zp_w = w.zero_point
+  
+  scale_b = b.scale
+  zp_b = b.zero_point
+  
+  scale_next, zero_point_next = calc_scale_zero_point(min_val=stat['min'], max_val=stat['max'])
+
+  # Perparing input by shifting
+  X = x.float() - zp_x
+  layer.weight.data = (scale_x * scale_w/scale_next)*(layer.weight.data - zp_w)
+  layer.bias.data = (scale_b/scale_next)*(layer.bias.data + zp_b)
+
+  # All int
+
+  x = layer(X) + zero_point_next
+    
+  # x = F.relu(x)
+  # x = torch.sin(x)
+
+  # Reset
+  layer.weight.data = W
+  layer.bias.data = B
+  
+  return x, scale_next, zero_point_next
+
+
+# --------------------------------------------- #
+# Functions
+# --------------------------------------------- #
+
+def update_stats(x, stats, key):
+    """Update Statistics for a given layer with Neural Network model, providing input data to be processed.
+    Params:
+    -------
+    :x: input data fetched from a Pytorch dataloader.\n
+    :stats: python dictionary object, containing pairs (layer name,stats values).\n
+    :key: python string object, name for getting and updating pait (layer name,stats values).\n
+
+    Return:
+    -------
+    :stats: dictionary with updated values.
+    """
+    max_val, _ = torch.max(x, dim=1)
+    min_val, _ = torch.min(x, dim=1)
+  
+  
+    if key not in stats:
+        stats[key] = {"max": max_val.sum(), "min": min_val.sum(), "total": 1}
+    else:
+        stats[key]['max'] += max_val.sum().item()
+        stats[key]['min'] += min_val.sum().item()
+        stats[key]['total'] += 1
+  
+    return stats
+
+def gather_activation_stats(model, x, stats):
+    """Gather Statistics for a given layer with Neural Network model, providing input data to be processed.
+    Params:
+    -------
+    :model: PyTorch like neural network model.\n
+    :x: input data fetched from a Pytorch dataloader.\n
+    :stats: python dictionary object, that will contain pairs (layer name,stats values).\n
+    
+    Return:
+    -------
+    :stats: dictionary with updated values.
+    """
+    for name_module, module_obj in model.named_modules():
+        if type(module_obj) == SineLayerCQ:
+            stats = update_stats(x.clone().view(x.shape[0], -1), stats, f'{name_module}')
+            x = module_obj(x)
+            pass
+        pass
+
+    return stats
+
+
+def gather_stats(model, test_loader, device = 'cpu'):
+    """Gather Statistics for a given layer with Neural Network model, providing input data to be processed.
+    Params:
+    -------
+    :model: PyTorch like neural network model.\n
+    :test_loader: input data fetched from a Pytorch dataloader.\n
+    :device: options allowed [cpu,gpu, or cuda].\n
+    
+    Return:
+    -------
+    :stats: dictionary with updated values.
+    """
+    
+    model.eval()
+    # test_loss = 0
+    # correct = 0
+    stats = {}
+    with torch.no_grad():
+        for data, target in test_loader:
+            if device == 'cpu':
+                data = data['coords'].to('cpu')
+                target = target['img'].to('cpu')
+            else:
+                data = data['coords'].cuda() # .to(device)
+                target = target['img'].cuda() # .to(device)
+                pass
+            stats = gather_activation_stats(model, data, stats)
+    
+    final_stats = {}
+    for key, value in stats.items():
+      final_stats[key] = { "max" : value["max"] / value["total"], "min" : value["min"] / value["total"] }
+    return copy.deepcopy(final_stats)
