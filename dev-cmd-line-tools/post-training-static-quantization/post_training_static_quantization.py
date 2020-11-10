c9a94002509d06022cdbde374734b54f6de026da
franec94 <franec94@gmail.com>
2020-11-10 18:18:28 +0100

    Minor changes.

--- dev-cmd-line-tools/post-training-static-quantization/post_training_static_quantization.py
+++ dev-cmd-line-tools/post-training-static-quantization/post_training_static_quantization.py
@@ -80,11 +80,17 @@ def process_posterior_quantization_mode(opt, root_path):
         df_filename = os.path.join(root_path, 'result_quant.csv')
         df_results.to_csv(f'{df_filename}')
 
-        print('Head results:')
+        print()
+        str_h = '=' * 50  + ' Head results: ' + '=' * 50
+        print('_' * str_h)
+        print(str_h)
         table = tabulate.tabulate(tabular_data=df_results.values[:5], headers=df_results.columns)
         print(table)
 
-        print('Tail results:')
+        print()
+        str_h = '=' * 50  + ' Tail results: ' + '=' * 50
+        print('_' * str_h)
+        print(str_h)
         table = tabulate.tabulate(tabular_data=df_results.values[-5:-1], headers=df_results.columns)
         print(table)
         pass
