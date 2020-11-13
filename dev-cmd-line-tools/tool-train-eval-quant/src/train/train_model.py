b35143e8ce7821e85f9ac57a39379ccd41cbf1cd
franec94 <franec94@gmail.com>
2020-11-13 11:29:31 +0100

    Minor changes.

--- dev-cmd-line-tools/tool-train-eval-quant/src/train/train_model.py
+++ dev-cmd-line-tools/tool-train-eval-quant/src/train/train_model.py
@@ -393,7 +393,7 @@ def train_model(opt, image_dataset, model_dir = '.', save_results_flag = False):
             # --- Show some infos from main function.
             table_vals = list(hyper_param_opt._asdict().items())
             table = tabulate.tabulate(table_vals, headers="Hyper-param,Value".split(","))
-            _log_infos(info_msg = f"{table}", header_msg = f'{"-" * 25} Model Details {"-" * 25}', logging=logging, tqdm=tqdm)
+            _log_infos(info_msg = f"{table}", header_msg = f'{"-" * 25} Model Details {"-" * 25}', logging=logging, tqdm=tqdm, verbose=verbose)
 
             train_h = "-" * 25 + " Train " + "-" * 25
             if opt.evaluate and n > 1:
