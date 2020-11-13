9ba2f4e1eb02ca70803ca0303c5b6e4d765df36f
franec94 <franec94@gmail.com>
2020-11-13 15:58:04 +0100

    Minor changes.

--- dev-cmd-line-tools/tool-train-eval-quant/run_program.py
+++ dev-cmd-line-tools/tool-train-eval-quant/run_program.py
@@ -13,10 +13,10 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
   --logging_root '../../../../results/cameramen' \
   --experiment_name 'train' \
   --sidelength 256 \
-  --num_epochs 100 \
-  --n_hf 16 \
-  --n_hl 5 6 \
-  --seed 0 1 \
+  --num_epochs 500000 \
+  --n_hf 64 \
+  --n_hl 5 6 7 8 9 10 11 12 13 14 \
+  --seed 0 \
   --cuda \
   --train \
   --evaluate \
